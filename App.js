2e47bac22a6f9288ef78c54cc9d63f928d3a752e
Mayuresh <mckakade@gmail.com>
2024-01-06 11:57:59 +0530

    ep-3

--- App.js
+++ App.js
@@ -1,30 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//Create following structure using react
-/*
-<div id="parent">
-<div id="child">
-    <h1>I'm h1 tag</h1>
-    <h2>I'm h1 tag</h2>
-</div>
-<div id="child2">
-    <h1>I'm h1 tag</h1>
-    <h2>I'm h2 tag</h2>
-</div>
-</div>
-*/
-
-const headings = [
-  React.createElement("h1", {}, "I am h1 tag"),
-  React.createElement("h2", {}, "I am h2 tag"),
-];
-const childDivs = [
-  React.createElement("div", { id: "child" }, headings),
-  React.createElement("div", { id: "child2" }, headings),
-];
-const parent = React.createElement("div", { id: "parent" }, childDivs);
-console.log(parent); // react element is js object.
+const reactElement = <span>My react element</span>;
+const Title = () => <h1>My Title 🚀</h1>;
+const Heading = () => (
+  <div>
+    <Title />
+    <h1>Heading in functional component 🚀</h1>
+    {reactElement}
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent); // render method is responsible for converting the react element into dom elements.
+root.render(<Heading />); // render method is responsible for converting the react element into dom elements.
